002746e0cb329236f0b3f790e4756c9bd1b22dc6
Christopher N. Hesse <raymanfx@gmail.com>
2015-08-12 16:15:58 +0200

    openmax: Add support for Samsung physical adress formats
    This enables hardware encoding of H.264 content for devices
    which need these formats.
    
    Strangely, OMX_SEC_COLOR_FormatNV12LPhysicalAddress does not seem
    to be linear, as disassembling the Samsung blobs has shown that
    this format is being converted to VIDEO_COLORFORMAT_NV12_TILED.
    
    Change-Id: Ic3fcd49f183484331618b12ac857e726cc24def8

--- osal/Exynos_OSAL_ETC.c
+++ osal/Exynos_OSAL_ETC.c
@@ -321,9 +321,11 @@ int Exynos_OSAL_OMX2VideoFormat(
         nVideoFormat = VIDEO_COLORFORMAT_NV12;
         break;
     case OMX_SEC_COLOR_FormatNV21Linear:
+    case OMX_SEC_COLOR_FormatNV21LPhysicalAddress:
         nVideoFormat = VIDEO_COLORFORMAT_NV21;
         break;
     case OMX_SEC_COLOR_FormatNV12Tiled:
+    case OMX_SEC_COLOR_FormatNV12LPhysicalAddress:
         nVideoFormat = VIDEO_COLORFORMAT_NV12_TILED;
         break;
     case OMX_COLOR_FormatYUV420Planar:
